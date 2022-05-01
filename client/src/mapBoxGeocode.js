b2d33706a3f0868a2fd3e750066d333694bcdf3f
Priscila Flores <priscilauchoaa@gmail.com>
2022-05-01 17:12:05 +0200

    fix redux

--- client/src/mapBoxGeocode.js
+++ client/src/mapBoxGeocode.js
@@ -1,54 +1,204 @@
+// import { useEffect, useState } from "react";
+// import mapboxgl from "mapbox-gl";
+// import mapboxGeocode from "@mapbox/mapbox-sdk/services/geocoding";
+// import TextField from "@mui/material/TextField";
+// import * as React from "react";
+// import Box from "@mui/material/Box";
+// import List from "@mui/material/List";
+// import ListItemButton from "@mui/material/ListItemButton";
+// import Button from "@mui/material/Button";
+// import ListItemText from "@mui/material/ListItemText";
+import { receveidNeedies } from "./redux/needies/slice.js";
+import { useDispatch } from "react-redux";
+// import "./mapBoxGeocode.css";
+import { useHistory } from "react-router-dom";
+
+// mapboxgl.accessToken =
+//     "pk.eyJ1IjoicHJpc2NpbGFmbG9yZXMiLCJhIjoiY2wyaHEyNnA0MGc5bzNjbm5ldm9zeWQwaCJ9.2wKNqQY375w7wVhbVi1PjQ";
+
+// export default function GeoSearch() {
+
+//     const [query, setQuery] = useState("");
+//     const [places, setPlaces] = useState([]);
+//     const [selectedIndex, setSelectedIndex] = useState();
+//     const [showList, setShowList] = useState(true);
+//     const [needies, setNeedies] = useState(true);
+//     const [labelInputAdress, setLabelInputAdress] = useState("Address");
+//     const [longit, setLongit] = useState("");
+//     const [latit, setLatit] = useState("");
+
+//     const handleListItemClick = (event) => {
+//         setSelectedIndex(event);
+//         setShowList(false);
+//         setLabelInputAdress(event.target.innerHTML);
+//         setLongit(places[0].geometry.coordinates[0]);
+//         setLatit(places[0].geometry.coordinates[1]);
+
+//         console.log("&&&&", selectedIndex);
+//     };
+
+//     const geocodeClient = mapboxGeocode({
+//         accessToken: mapboxgl.accessToken,
+//     });
+
+//     const handleIntputChange = (e) => {
+//         setLabelInputAdress("");
+
+//         setShowList(true);
+//         setQuery(e.target.value);
+//     };
+//     console.log("places", places);
+
+//     useEffect(() => {
+//         if (query.length < 3) return;
+
+//         // geocoding with countries
+//         geocodeClient
+//             .forwardGeocode({
+//                 query,
+//                 countries: ["de"],
+//                 limit: 5,
+//             })
+//             .send()
+//             .then((response) => {
+//                 setPlaces(response.body.features);
+//                 // setType(places[0].type);
+//             });
+//     }, [query]);
+//     const handleClick = () => {
+//         // console.log("*****", longit, latit);
+//         fetch(`/api/needies/${longit}/${latit}`)
+//             .then((res) => res.json())
+//             .then(({ rows }) => {
+//                 console.log("data++++", rows);
+//                 if (!rows) {
+//                     console.log("No needies in this address");
+//                     history.push("/");
+//                 } else {
+//                     console.log("needies", rows);
+//                     setNeedies(rows);
+//                     console.log("needies fetched", needies);
+//
+//                 }
+//             });
+//     };
+//     console.log("places", places);
+//     return (
+//         <div className="geo-container">
+//             <TextField
+//                 id="adreess-input"
+//                 label="Address"
+//                 variant="standard"
+//                 value={query}
+//                 placeholder="Gartenstrasse 18, 10115 Berlin"
+//                 onChange={handleInputChange}
+//             />
+//             <Button onClick={handleSubmitClick}> Submit</Button>
+//             {showList && (
+//                 <Paper className="address-list" elevation={3}>
+//                     <List component="nav" aria-label="main mailbox folders">
+//                         {places.map((place) => (
+//                             <ListItemButton key={place.id}>
+//                                 <ListItemText
+//                                     primary={place.place_name}
+//                                     onClick={() => handleListItemClick(place)}
+//                                 />
+//                             </ListItemButton>
+//                         ))}
+//                     </List>
+//                 </Paper>
+//             )}
+//         </div>
+//     );
+// }
+
+// // const [placeName, setPlaceName] = useState();
+
+// // const [type, setType] = useState("");
+// // const [typeGeometry, setTypeGeometry] = useState("");
+// // const handleClick = () => {
+// // setType(places[0].type);
+// // setTypeGeometry(places[0].geometry.type);
+// // placeName(places[0].place_name);
+
+// //     setLabelInputAdress("");
+// //     fetch("/api/locations", {
+// //         method: "POST",
+// //         headers: {
+// //             "Content-Type": "application/json",
+// //         },
+// //         body: JSON.stringify({
+// //             name: "Berlin",
+// //             geoJSON: {
+// //                 type: type,
+// //                 geometry: {
+// //                     type: typeGeometry,
+// //                     coordinates: [long, lat],
+// //                 },
+// //             },
+// //         }),
+// //     });
+// // };
+
 import { useEffect, useState } from "react";
-import mapboxgl from "mapbox-gl";
 import mapboxGeocode from "@mapbox/mapbox-sdk/services/geocoding";
 import TextField from "@mui/material/TextField";
-import * as React from "react";
-import Box from "@mui/material/Box";
 import List from "@mui/material/List";
 import ListItemButton from "@mui/material/ListItemButton";
 import Button from "@mui/material/Button";
 import ListItemText from "@mui/material/ListItemText";
-import { receveidNeedies } from "./redux/needies/slice.js";
-import { useDispatch } from "react-redux";
-import "./mapBoxGeocode.css";
-import { useHistory } from "react-router-dom";
+import Paper from "@mui/material/Paper";
 
-mapboxgl.accessToken =
+import "./mapBoxGeocode.css";
+
+const geocodeAccessToken =
     "pk.eyJ1IjoicHJpc2NpbGFmbG9yZXMiLCJhIjoiY2wyaHEyNnA0MGc5bzNjbm5ldm9zeWQwaCJ9.2wKNqQY375w7wVhbVi1PjQ";
+const geocodeClient = mapboxGeocode({
+    accessToken: geocodeAccessToken,
+});
 
 export default function GeoSearch() {
     const history = useHistory();
     const dispatch = useDispatch();
     const [query, setQuery] = useState("");
     const [places, setPlaces] = useState([]);
-    const [selectedIndex, setSelectedIndex] = useState();
     const [showList, setShowList] = useState(true);
     const [needies, setNeedies] = useState(true);
-    const [labelInputAdress, setLabelInputAdress] = useState("Address");
     const [longit, setLongit] = useState("");
     const [latit, setLatit] = useState("");
 
-    const handleListItemClick = (event) => {
-        setSelectedIndex(event);
-        setShowList(false);
-        setLabelInputAdress(event.target.innerHTML);
-        setLongit(places[0].geometry.coordinates[0]);
-        setLatit(places[0].geometry.coordinates[1]);
+    console.log("places", places);
 
-        console.log("&&&&", selectedIndex);
+    const handleListItemClick = (place) => {
+        setShowList(false);
+        setQuery(place.place_name);
+        setLongit(place.geometry.coordinates[0]);
+        setLatit(place.geometry.coordinates[1]);
     };
 
-    const geocodeClient = mapboxGeocode({
-        accessToken: mapboxgl.accessToken,
-    });
-
-    const handleIntputChange = (e) => {
-        setLabelInputAdress("");
-
+    const handleInputChange = (e) => {
         setShowList(true);
         setQuery(e.target.value);
     };
-    console.log("places", places);
+
+    const handleSubmitClick = () => {
+        // console.log("*****", longit, latit);
+        fetch(`/api/needies/${longit}/${latit}`)
+            .then((res) => res.json())
+            .then(({ rows }) => {
+                console.log("data++++", rows);
+                if (!rows) {
+                    console.log("No needies in this address");
+                    history.push("/");
+                } else {
+                    console.log("needies", rows);
+                    setNeedies(rows);
+                    console.log("needies fetched", needies);
+                    dispatch(receveidNeedies(rows));
+                    history.push("/needies");
+                }
+            });
+    };
 
     useEffect(() => {
         if (query.length < 3) return;
@@ -63,71 +213,31 @@ export default function GeoSearch() {
             .send()
             .then((response) => {
                 setPlaces(response.body.features);
-                // setType(places[0].type);
             });
     }, [query]);
-    const handleClick = () => {
-        // console.log("*****", longit, latit);
-        fetch(`/api/needies/${longit}/${latit}`)
-            .then((res) => res.json())
-            .then(({ rows }) => {
-                console.log("data++++", rows);
-                if (!rows) {
-                    console.log("No needies in this address");
-                    history.push("/");
-                } else {
-                    console.log("needies", rows);
-                    setNeedies(rows);
-                    console.log("needies fetched", needies);
-                    dispatch(receveidNeedies(rows));
 
-                    history.push("/needies");
-                }
-            });
-    };
-    console.log("places", places);
     return (
         <div className="geo-container">
-            <div>
-                <TextField
-                    id="adreess-input"
-                    label={labelInputAdress}
-                    variant="standard"
-                    defaultValue={labelInputAdress}
-                    onChange={handleIntputChange}
-                />
-                {/* {places.length > 0 && (
-                    <ul className="map-query-options">
-                        {places.map((place) => (
-                            <li key={place.id}> {place.place_name} </li>
-                        ))}
-                    </ul>
-                )} */}
-            </div>
+            <TextField
+                id="adreess-input"
+                label="Address"
+                variant="standard"
+                value={query}
+                placeholder="Gartenstrasse 18, 10115 Berlin"
+                onChange={handleInputChange}
+            />
+            <Button onClick={handleSubmitClick}> Submit</Button>
             {showList && (
-                <Box
-                    sx={{
-                        width: "100%",
-                        maxWidth: 360,
-                        bgcolor: "background.paper",
-                    }}
-                >
-                    <List component="nav" aria-label="main mailbox folders">
-                        {places.map((place) => (
-                            <ListItemButton key={place.id}>
-                                <ListItemText
-                                    primary={place.place_name}
-                                    //passing place but receving
-                                    onClick={(place) =>
-                                        handleListItemClick(place)
-                                    }
-                                />
-                            </ListItemButton>
-                        ))}
-                    </List>
-                </Box>
+                <ul>
+                    {places.map((place) => (
+                        <li className="list-places" key={place.id}>
+                            <p onClick={() => handleListItemClick(place)}>
+                                {place.place_name}
+                            </p>
+                        </li>
+                    ))}
+                </ul>
             )}
-            <Button onClick={handleClick}> Submit</Button>
         </div>
     );
 }
@@ -136,7 +246,7 @@ export default function GeoSearch() {
 
 // const [type, setType] = useState("");
 // const [typeGeometry, setTypeGeometry] = useState("");
-// const handleClick = () => {
+// const handleSubmitClick = () => {
 // setType(places[0].type);
 // setTypeGeometry(places[0].geometry.type);
 // placeName(places[0].place_name);
--- server/needies.sql
+++ server/needies.sql
@@ -7,19 +7,37 @@ CREATE TABLE needies (
   geom geometry,
   needy VARCHAR(255) NOT NULL, 
   category VARCHAR(255) NOT NULL,
-  description VARCHAR(255) NOT NULL
+  description VARCHAR(255) NOT NULL,
+  img TEXT
 );
 
-INSERT INTO needies (name, needy, category, description, geom)
-VALUES ('Berlin', 'MSF-Berlin', 'Donation', 'NGO Needs Winter Cloths', ST_GeomFromGeoJSON('{
-  "type": "Point",
-  "coordinates": [13.38333, 52.51667],
-}'));
-INSERT INTO needies (name, needy, category, description, geom)
-VALUES ('Alexander Platz', 'NGO-Animal Rescue', 'Volunteer Work', 'Rescue Animals', ST_GeomFromGeoJSON('{
+
+
+INSERT INTO needies (name, needy, category, description,img, geom)
+VALUES ('Alexander Platz', 'NGO-Animal Rescue', 'Volunteer Work', 'Rescue Animals', 'https://www.thebalancecareers.com/thmb/7E7vjaHSUolVe5YtPKmqY8mcIWs=/2000x1500/filters:fill(auto,1)/GettyImages-660747797-5892560b3df78caebcbf9e0d.jpg', ST_GeomFromGeoJSON('{
   "type": "Point",
   "coordinates": [13.412599, 52.521534625],
 }'));
+
+
+INSERT INTO needies (name, needy, category, description, img, geom)
+VALUES ('Mitte', 'DRK - Deutsches Rotes Kreuz', 'Donation', 'Needs Winter Cloths','https://res.cloudinary.com/updater-marketing/images/f_auto,q_auto/v1617747591/shutterstock_1733375798/shutterstock_1733375798.png?_i=AA', ST_GeomFromGeoJSON('{
+  "type": "Point",
+  "coordinates": [13.38333, 52.51667],
+}'));
+
+INSERT INTO needies (name, needy, category, description,img, geom)
+VALUES ('Wedding Platz', 'Become a member', 'Volunteer Work', 'Rescue Animals','https://cdn.hswstatic.com/gif/animal-rescue-organizations-1.jpg', ST_GeomFromGeoJSON('{
+  "type": "Point",
+  "coordinates": [13.361902, 52.540838],
+}'));
+
+
+
+
+
+
+
 
 
 
